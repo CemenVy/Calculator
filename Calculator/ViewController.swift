49df877bd42a4a4705b61b90dc91842ecfc3028e
CemenVy <semen.vydrin0108@gmail.com>
2023-07-24 22:37:54 +0400

    Work with UI

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var calcButtons: [UIButton]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for button in calcButtons {
+            button.layer.cornerRadius = button.frame.size.height / 2;
+            
+        }
+        
     }
 
 
