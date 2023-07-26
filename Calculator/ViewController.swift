b3b26f39258b7088385a8f98f98040159ca5fea1
CemenVy <semen.vydrin0108@gmail.com>
2023-07-26 20:00:47 +0400

    Work with logic

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,49 +9,46 @@ import UIKit
 
  class ViewController: UIViewController {
     
-    
-    @IBOutlet var displayResultLabel: UILabel!
-    @IBOutlet var buttonsOnCalculator: [UIButton]!
+     // MARK: - IB Outlets
+     @IBOutlet private var displayResultLabel: UILabel!
+     @IBOutlet private var buttonsOnCalculator: [UIButton]!
      
-    private var stillTyping = false
-    private var dotIsPlaced = false
-    private var firstOperand = 0.0
-    private var secondOperand = 0.0
-    private var operationSign = ""
-    
-    var currentInput: Double {
-        get {
-            return Double(displayResultLabel.text!) ?? 0
-        }
-        
-        set {
-            let value = "\(newValue)"
-            let values = value.components(separatedBy: ".")
-            
-            if values [1] == "0" {
-                displayResultLabel.text = "\(values[0])"
-            } else {
-                displayResultLabel.text = "\(newValue)"
-            }
-            
-            stillTyping = false
-        }
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
+     // MARK: - Private Properties
+     private var stillTyping = false
+     private var dotIsPlaced = false
+     private var firstOperand = 0.0
+     private var secondOperand = 0.0
+     private var operationSign = ""
+     
+     private var currentInput: Double {
+         get {
+             return Double(displayResultLabel.text!) ?? 0
+         }
+         
+         set {
+             let value = "\(newValue)"
+             let values = value.components(separatedBy: ".")
+             
+             if values [1] == "0" {
+                 displayResultLabel.text = "\(values[0])"
+             } else {
+                 displayResultLabel.text = "\(newValue)"
+             }
+             
+             stillTyping = false
+         }
+     }
+     
+    // MARK: - View Life Cycles
     
     override func viewWillLayoutSubviews() {
-        
         for button in buttonsOnCalculator {
             button.layer.cornerRadius = button.frame.size.height / 2
         }
-        
     }
     
-    @IBAction func numbersOnCalculatorButtonsPressed(_ sender: UIButton) {
+    // MARK: - IB Action
+    @IBAction private func numbersOnCalculatorButtonsPressed(_ sender: UIButton) {
         let number = sender.currentTitle
         
         if stillTyping {
@@ -64,19 +61,14 @@ import UIKit
         }
     }
     
-    @IBAction func twoOperandsSignPressed(_ sender: UIButton) {
+    @IBAction private func twoOperandsSignPressed(_ sender: UIButton) {
         operationSign = sender.currentTitle!
         firstOperand = currentInput
         stillTyping = false
         dotIsPlaced = false
     }
     
-    private func operateWithToOperands(_ closure: (Double, Double) -> Double) {
-        currentInput = closure(firstOperand, secondOperand)
-    }
-    
-    @IBAction func equalitySignPressed(_ sender: UIButton) {
-        
+    @IBAction private func equalitySignPressed(_ sender: UIButton) {
         if stillTyping {
             secondOperand = currentInput
         }
@@ -93,11 +85,10 @@ import UIKit
         case "-":
             operateWithToOperands {$0 - $1}
         default: break
-            
         }
     }
      
-     @IBAction func clearButtonPressed(_ sender: UIButton) {
+     @IBAction private func clearButtonPressed(_ sender: UIButton) {
          firstOperand = 0
          secondOperand = 0
          currentInput = 0
@@ -107,11 +98,11 @@ import UIKit
          operationSign = ""
      }
      
-     @IBAction func plusMinusButtonPressed(_ sender: UIButton) {
+     @IBAction private func plusMinusButtonPressed(_ sender: UIButton) {
          currentInput = -currentInput
      }
      
-     @IBAction func percentageButtonPressed(_ sender: UIButton) {
+     @IBAction private func percentageButtonPressed(_ sender: UIButton) {
          if firstOperand == 0 {
              currentInput = currentInput / 100
          } else {
@@ -120,7 +111,7 @@ import UIKit
          stillTyping = false
      }
      
-     @IBAction func dotButtonPressed(_ sender: UIButton) {
+     @IBAction private func dotButtonPressed(_ sender: UIButton) {
          if stillTyping && !dotIsPlaced {
              displayResultLabel.text = displayResultLabel.text! + "."
              dotIsPlaced = true
@@ -129,5 +120,11 @@ import UIKit
          }
      }
      
+     // MARK: - Private Methods
+     private func operateWithToOperands(_ closure: (Double, Double) -> Double) {
+         currentInput = closure(firstOperand, secondOperand)
+     }
+     
  }
+
 
